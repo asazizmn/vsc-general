c56ec432b271fc4733511a6f4ae3e511b60ee4a3
S. Aziz <asazizmn@gmail.com>
2022-08-25 17:39:15 +0800

    test long to float conversion

--- OODConcepts/src/Primitives.java
+++ OODConcepts/src/Primitives.java
@@ -13,16 +13,19 @@ public class Primitives {
         int intVar = 2_147_483_647;
         System.out.println(intVar);
 
-        long longVar = 2_147_483_647_000L;
+        // long longVar = 2_147_483_647_000_000_000L;
+        long longVar = 2_147_483_647_999_999_999L;
         System.out.println(longVar);
+        System.out.println((float)longVar);
 
 
         // how to calculate range of float variables
         // https://www.youtube.com/watch?v=YYIeMM8By6Y&t=174s
-        float floatVar = 23.0F;
+        // float floatVar = 2_147_483_647_000_000_000_000_000_000_000_000_000F;
+        float floatVar = 32.0F;
         System.out.println(floatVar);
 
-        double doubleVar = 23.0;
+        double doubleVar = 64.0;
         System.out.println(doubleVar);
     }
 }
